a544a481991bcf1baf177a987b45a33ccff30789
AvetisDN <avetisdn@gmail.com>
2020-02-15 16:39:06 +0300

    booking section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 document.querySelector('#burger').onclick = function(e) {
     document.querySelector('#menu').classList.toggle('show');
 }
+
+jQuery('#book-date').datetimepicker({
+    timepicker: false,
+    format:'d.m.Y',
+})
+jQuery('#book-time').datetimepicker({
+    datepicker: false,
+    format:'H:i',
+    allowTimes:[
+        '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', 
+        '16:00', '17:00', '18:00', '19:00', '20:00'
+       ]
+})
